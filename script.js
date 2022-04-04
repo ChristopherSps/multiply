dfb09b1aa9c4a46fe1beb6a9c8b400c63b8a7f56
ChristopherSps <98600618+ChristopherSps@users.noreply.github.com>
2022-04-04 13:11:40 -0400

    Fix X and Y

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-// Global Variables
 
-
-let mult = document.createElement("h1");
-document.body.appendChild(mult);
-// mult.innerText("Simple Multiplication");
-display();
 
 function display(){
-	let question = document.createElement("div");
-	question.setAttribute("id","question");
-	document.body.appendChild(question);
+	// Creating Interface
+	let interface = document.createElement("div");
+	interface.setAttribute("id","interface");
+	document.body.appendChild(interface);
 
-	let answer = document.createElement("input");
-	answer.setAttribute("id","input");
-	document.body.appendChild(answer);
+	// Creating Heading
+	let heading = document.createElement("h1");
+	heading.setAttribute("id","heading");
+	heading.innerText = "Simple Multiplication";
+	interface.appendChild(heading);
 
-	let button = document.createElement("button");
-	button.setAttribute("onClick","button");
-	button.setAttribute("onClick","check()");
-	document.body.appendChild(button);
-	button.innerText("submit");
+	// Creating Paragraph
+	let para = document.createElement("p");
+	para.setAttribute("id","para");
+	para.innerText = askQuestion();
+	interface.appendChild(para);
+
+	// Creating Input Box
+	let iBox = document.createElement("input");
+	iBox.setAttribute("id","iBox");
+	interface.appendChild(iBox);
+
+	//Creating Button
+	let submitB = document.createElement("button");
+	submitB.setAttribute("id","submit");
+	submitB.setAttribute("onClick","checkAnswer()");
+	submitB.innerText = "Check Answer";
+	interface.appendChild(submitB);
 }
 
-function check(){
+// Global Variables
+var x = 0;
+var y = 0;
+var product = 0;
+var min = 3;
+var max = 10;
 
+function askQuestion(){
+	let x = Math.floor(Math.random()*10)+1;
+	let y = Math.floor(Math.random()*10)+1;
+	product = (x * y);
+	let question =  x+" X "+y+" = ?";
+	return question;
+}
+
+function checkAnswer(){
+	let answer = document.getElementById("iBox");
+	let answerBox = answer.value;
+	if (answerBox == product){
+		alert("Correct, "+x+" X "+y+" is "+product);
+		document.location = 'index.html';
+	}
+	else{
+		alert("Incorrect! "+x+" X "+y+" is "+product);
+		document.location = 'index.html';
+	}
 }
